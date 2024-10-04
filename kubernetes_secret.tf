258ef9ae9e3b22bc8b8a64c8fdc3eb8864dc726d
DaShaun <dashaun@dashaun.com>
2024-10-03 22:02:01 -0500

    pub keys

--- kubernetes_secret.tf
+++ kubernetes_secret.tf
@@ -11,7 +11,7 @@ resource "kubernetes_secret" "concourse_web" {
     local-users             = "test:test"
     session-signing-key     = tls_private_key.concourse_session_signing_key.private_key_pem
     vault-client-auth-param = format("role_id:%s,secret_id:%s", vault_approle_auth_backend_role.concourse.role_id, vault_approle_auth_backend_role_secret_id.concourse.secret_id)
-    worker-key-pub          = tls_private_key.concourse_worker_key.public_key_pem
+    worker-key-pub          = tls_private_key.concourse_worker_key.public_key_openssh
   }
 
   type = "Opaque"
@@ -24,9 +24,9 @@ resource "kubernetes_secret" "concourse_worker" {
   }
 
   data = {
-    host-key-pub   = tls_private_key.concourse_host_key.public_key_pem
+    host-key-pub   = tls_private_key.concourse_host_key.public_key_openssh
     worker-key     = tls_private_key.concourse_worker_key.private_key_pem
-    worker-key-pub = tls_private_key.concourse_worker_key.public_key_pem
+    worker-key-pub = tls_private_key.concourse_worker_key.public_key_openssh
   }
 
   type = "Opaque"
